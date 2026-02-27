55d8c7154449419e139004d01eda09716ac82cbd
Eduardo Roskoski <eduardo.roskoski.oliveira@escola.pr.gov.br>
2026-02-27 10:27:50 -0300

    python.py

--- python.py
+++ python.py
@@ -1,7 +1,6 @@
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
-
 numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
@@ -23,7 +22,7 @@ while (rodada <= total_de_tentativas):
         if(maior):
             print("O seu chute foi maior do que o número secreto!")
         elif(menor):
-        print("O seu chute foi menor do que o número secreto!")
+            print("O seu chute foi menor do que o número secreto!")
 
     rodada = rodada + 1
 
